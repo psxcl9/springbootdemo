4b5868e407560ad0b6b2a3f457136d0b295cee36
liaocx <whoamicxl@gmail.com>
2019-01-04 11:28:50 +0800

    完善pom依赖

---
+++ demo-common/src/main/java/com/liaocx/convert/DateSerializer.java
@@ -0,0 +1,23 @@
+package com.liaocx.convert;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Objects;
+
+public class DateSerializer extends JsonSerializer {
+    @Override
+    public void serialize(Object arg0, JsonGenerator arg1, SerializerProvider arg2)
+            throws IOException, JsonProcessingException {
+
+        if(!Objects.isNull(arg0)) {
+            Date value=(Date)arg0;
+            arg1.writeNumber(value.getTime());
+        }
+
+    }
+}
